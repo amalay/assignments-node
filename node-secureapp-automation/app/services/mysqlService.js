cd795a5b5b3d11c681f570225bc93945c200492a
Amalay <84455469+amalay@users.noreply.github.com>
2021-08-12 22:15:19 +0530

    Updated

--- node-secureapp-automation/app/services/mysqlService.js
+++ node-secureapp-automation/app/services/mysqlService.js
@@ -1,19 +1,34 @@
 'use strict';
 const mysql = require('mysql2');
 const env = require('dotenv').config();
-const dbConn = mysql.createConnection({ host: process.env.MYSQL_HOST, user: process.env.MYSQL_ROOT_USER, password: process.env.MYSQL_ROOT_PASSWORD, database: process.env.MYSQL_DATABASE });
+const dbConn = mysql.createConnection({ host: process.env.MYSQL_HOST, user: process.env.MYSQL_ROOT_USER, password: process.env.MYSQL_ROOT_PASSWORD, multipleStatements: true });
 
 dbConn.connect(function(err) {
-  if (err){
-    throw err;
-  } 
-  
-  console.log("Mysql database connected successfully!");
+    if (err){
+        throw err;
+    } 
+    
+    console.log("Mysql database connected successfully!");
 
-  /* var personTable = `CREATE TABLE IF NOT EXISTS person (
-    Id INT PRIMARY KEY AUTO_INCREMENT,
-    FirstName VARCHAR(25) NOT NULL,
-    LastName VARCHAR(25) NOT NULL)`;
+    var sqlScript = `
+    CREATE DATABASE IF NOT EXISTS ${process.env.MYSQL_DATABASE}; 
+    
+    USE ${process.env.MYSQL_DATABASE};
 
-    dbConn.query(personTable); */
+    CREATE TABLE IF NOT EXISTS person 
+    (
+        Id INT PRIMARY KEY AUTO_INCREMENT,
+        FirstName VARCHAR(25) NOT NULL,
+        LastName VARCHAR(25) NOT NULL
+    )
+
+    `;
+
+    dbConn.query(sqlScript, (err, result) => {
+        if(err) {                    
+            throw err;
+        }
+
+        console.log(`MySql script executed successfully!`);        
+    });
 });
