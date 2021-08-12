c733c4837bd51a31631074d58446a301d9a6f167
Amalay <84455469+amalay@users.noreply.github.com>
2021-08-12 21:38:59 +0530

    Updated

--- node-secureapp-automation/app/services/mysqlService.js
+++ node-secureapp-automation/app/services/mysqlService.js
@@ -1,5 +1,6 @@
 'use strict';
 const mysql = require('mysql2');
+const env = require('dotenv').config();
 const dbConn = mysql.createConnection({ host: process.env.MYSQL_HOST, user: process.env.MYSQL_ROOT_USER, password: process.env.MYSQL_ROOT_PASSWORD, database: process.env.MYSQL_DATABASE });
 
 dbConn.connect(function(err) {
@@ -8,4 +9,11 @@ dbConn.connect(function(err) {
   } 
   
   console.log("Mysql database connected successfully!");
+
+  /* var personTable = `CREATE TABLE IF NOT EXISTS person (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(25) NOT NULL,
+    LastName VARCHAR(25) NOT NULL)`;
+
+    dbConn.query(personTable); */
 });
